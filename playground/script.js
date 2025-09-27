3eafb921ebc6e6b58c2587a144915b2bda0d2378
maxime paquet <maxime.paquet.89@gmail.com>
2025-09-27 23:44:33 +0200

    root folder

---
+++ playground/script.js
@@ -0,0 +1,70 @@
+// console.log("Hello from Node.js");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+
+// rl.question('Are you the boss? (yes/no) ', (answer) => {
+//     if (answer.trim().toLowerCase() === 'yes' || answer.trim().toLowerCase() === 'y') {
+//         rl.question('How old are you? ', (age) => {
+//             console.log(age);
+//             rl.close();
+//         });
+//     } else {
+//         console.log('Access denied.');
+//         rl.close();
+//     }
+// }
+// );
+
+rl.question('Are you the boss? (yes/no) ', function (answer) {
+    if (answer.trim().toLowerCase() === 'yes' || answer.trim().toLowerCase() === 'y') {
+        rl.question('How old are you? ', function (age) {
+            console.log(age);
+            rl.close();
+        });
+    } else {
+        console.log('Access denied.');
+        rl.close();
+    }
+});
+
+
+// let sum = 0;
+
+// function ask(sum) {
+//     if (sum >= 20) {
+//         console.log('Sum: ' + sum);
+//         rl.close();
+//         return;
+//     }
+//     rl.question('Enter a number (empty or 0 to finish): ', (input) => {
+//         const value = Number(input);
+//         if (!input || !value) {
+//             console.log('Sum: ' + sum);
+//             rl.close();
+//             return;
+//         }
+//         sum += value;
+//         ask(sum);
+//     });
+// }
+
+// ask(sum);
+
+
+// function sayHi(name) {   // (1) create
+//     str_ = "Hello" + ' ' + name
+//     console.log(str_);
+//     return str_
+// }
+
+// let func = sayHi;    // (2) copy
+
+// res = func("Max"); // Hello     // (3) run the copy (it works)!
+
+// console.log("After fun called\n" + res)
