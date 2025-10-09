17f238c06d1a483421e914d77079a1837a4a1018
maxime paquet <maxime.paquet.89@gmail.com>
2025-10-09 23:46:45 +0200

    lifespan

---
+++ backend/chatbot/api/lifespan.py
@@ -0,0 +1,38 @@
+from contextlib import asynccontextmanager
+from dataclasses import dataclass
+from fastapi import Request, FastAPI
+from pathlib import Path
+
+from langgraph.checkpoint.base import BaseCheckpointSaver
+from langgraph.pregel import Pregel
+
+from chatbot.agent.main import get_agent
+
+HERE = Path(__file__).parent
+
+
+@dataclass
+class AgentLifespanState:
+    """Global state for the lifespan of the application."""
+
+    agent: Pregel
+    checkpointer: BaseCheckpointSaver
+
+
+def get_state(request: Request) -> AgentLifespanState:
+    return request.app.state.state
+
+
+@asynccontextmanager
+async def lifespan(
+    app: FastAPI,
+):
+    from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
+
+    CHECKPOINT_SQLITE = f"{HERE}/../../data/checkpoint_app.db"
+    async with AsyncSqliteSaver.from_conn_string(CHECKPOINT_SQLITE) as checkpointer:
+        await checkpointer.setup()
+        agent = get_agent(checkpointer=checkpointer, verbose=False)
+        app.state.state = AgentLifespanState(agent=agent, checkpointer=checkpointer)
+
+        yield
