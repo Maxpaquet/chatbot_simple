ad3a580ca20f099df2056511fb0ed5296a7e46d0
maxime paquet <maxime.paquet.89@gmail.com>
2025-10-16 21:33:51 +0200

    dev script for testing purposes

---
+++ backend/scripts/dev_mock.py
@@ -0,0 +1,23 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+
+from chatbot.agent.main import get_agent
+from chatbot.agent.mock_agent import AnsweringStateMock
+
+if __name__ == "__main__":
+    agent = get_agent(checkpointer=None, mock=True)
+    print("Mock agent created successfully.")
+
+    state = AnsweringStateMock(
+        messages=[
+            SystemMessage(content="You are a helpful assistant."),
+            HumanMessage(content="Human message mock"),
+        ],
+        answer=None,
+        remaining_steps=10,
+    )
+
+    final_state = agent.invoke(state)
+    print("Final state after running the mock agent:")
+    # print(final_state)
+    for m in final_state["messages"]:
+        m.pretty_print()
