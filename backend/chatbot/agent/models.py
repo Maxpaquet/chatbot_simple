04596c886d4c3ce5b4b1e5e601745df4545f0da8
maxime paquet <maxime.paquet.89@gmail.com>
2025-10-18 10:43:07 +0200

    type using List instead of list

--- backend/chatbot/agent/models.py
+++ backend/chatbot/agent/models.py
@@ -1,7 +1,8 @@
 from typing_extensions import TypedDict
-from typing import Annotated, Optional
+from typing import Annotated, Optional, List
 
 from langgraph.graph.message import add_messages, AnyMessage
+from langchain_core.messages import BaseMessage
 
 
 class Answer(TypedDict):
@@ -9,6 +10,6 @@ class Answer(TypedDict):
 
 
 class AnsweringState(TypedDict):
-    messages: Annotated[list[AnyMessage], add_messages]
+    messages: Annotated[List[AnyMessage], add_messages]
     answer: Optional[Answer]
     remaining_steps: int
