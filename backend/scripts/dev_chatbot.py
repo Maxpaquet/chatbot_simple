13f1b3d6e83bab594899da0b9d457dd5bcef41de
maxime paquet <maxime.paquet.89@gmail.com>
2025-12-23 19:52:28 +0100

    Refactor dev_chatbot.py for improved organization; streamline imports and enhance main function structure

--- backend/scripts/dev_chatbot.py
+++ backend/scripts/dev_chatbot.py
@@ -1,66 +1,63 @@
-from typing import List, Optional
-from typing_extensions import TypedDict
 from pathlib import Path
+from typing import Dict, List, Optional
 from uuid import uuid4
 
-from langchain_core.tools import BaseTool
-from langchain_core.messages import HumanMessage, BaseMessage, SystemMessage
-from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
 from langchain_core.runnables import RunnableConfig
+from langchain_core.tools import BaseTool
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.pregel import Pregel
+from typing_extensions import TypedDict
 
-from chatbot.agent.answering import create_agent, get_tools, AnsweringState
-from chatbot.utils import aget_model
-from chatbot.services.utils import prep_input
+from chatbot import agent
+from chatbot.agent.answering import AnsweringState, create_agent, get_tools
+from chatbot.agent.main import get_agents_dict
+from chatbot.config import config
+from chatbot.services.utils import prep_config, prep_input
+from chatbot.utils import get_model
 
 HERE = Path(__file__).parent
 
 CHECKPOINT_SQLITE = f"{HERE}/../data/checkpoint.db"
 
-if __name__ == "__main__":
-    llm = aget_model("gemini-flash-lite", temperature=0.0)
-    tools: List[BaseTool] = get_tools(verbose=True)
 
+async def main():
     thread_id = str(uuid4())
-    user_id = "user_1"
-    config = RunnableConfig(configurable={"thread_id": thread_id, "user_id": user_id})
+    checkpointer = None
+    # with SqliteSaver.from_conn_string(CHECKPOINT_SQLITE) as checkpointer:
+    agents_dict: Dict[str, Pregel] = await get_agents_dict(
+        checkpointer,
+        store=None,
+        k=5,
+        verbose=config.verbose,
+        mock=config.mock,
+    )
+    agent: Pregel = agents_dict["default"]
 
-    with SqliteSaver.from_conn_string(CHECKPOINT_SQLITE) as checkpointer:
-        agent = create_agent(
-            llm,
-            tools,
-            store=None,
-            checkpointer=checkpointer,
-        )
+    input_state = AnsweringState(
+        messages=[HumanMessage(content="What is the capital of France?")],
+        answer=None,
+        documents=None,
+        remaining_steps=10,
+    )
 
-        messages: List[BaseMessage] = [
-            SystemMessage(
-                content="You are a helpful assistant that helps people find information."
-            ),
-            HumanMessage(content="What is the capital of France?"),
-        ]
-        state_1 = AnsweringState(
-            messages=messages,
-            answer=None,
-            remaining_steps=10,
-        )
-        res = agent.invoke(state_1, config=config)
+    config_langchain: RunnableConfig = await prep_config(
+        thread_id, config.langfuse_config
+    )
 
-        print(res)
-        for m in res["messages"]:
-            m.pretty_print()
+    res = await agent.ainvoke(
+        input_state,
+        config=config_langchain,
+    )
+    # print(res)
+    print("*" * 100)
+    for m in res["messages"]:
+        m.pretty_print()
 
-        state_2 = AnsweringState(
-            messages=[
-                HumanMessage(
-                    content="Repeat the name of the city and tell me what can I do in this city ?"
-                )
-            ],
-            answer=None,
-            remaining_steps=10,
-        )
 
-        res = agent.invoke(state_2, config=config)
-        print("*" * 100)
-        print(res)
-        for m in res["messages"]:
-            m.pretty_print()
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
