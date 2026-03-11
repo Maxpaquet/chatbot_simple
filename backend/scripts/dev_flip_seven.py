252593b87d123a999d482400095b0e51871596d2
maxime paquet <maxime.paquet.89@gmail.com>
2026-03-11 21:58:32 +0100

    scripts for dev purposes

--- backend/scripts/dev_flip_seven.py
+++ backend/scripts/dev_flip_seven.py
@@ -9,6 +9,25 @@ from chatbot.flip_seven.game import (
 from chatbot.flip_seven.models import Action, GameState, PlayerState, QTable
 from chatbot.flip_seven.player import draw_player, random_player
 from chatbot.flip_seven.strategy import QLearningAgent, q_learning_strategy
+import os
 
 if __name__ == "__main__":
-    print(q_learning_strategy(1_000_000, max_nb_cards=3, max_card_value=8, debug=False))
+    # qtable: QTable = q_learning_strategy(
+    #     10_000_000,
+    #     max_nb_cards=7,
+    #     max_card_value=13,
+    #     debug=False,
+    #     epsilon_percentage=0.03,
+    # )
+    qtable: QTable = QTable.empty()
+    qtable.load_q_table("q_table.pkl")
+
+    print(qtable)
+    output_path = "q_table.txt"
+    # Ensure the file exists (will create if not)
+    if not os.path.exists(output_path):
+        open(output_path, "w").close()
+
+    with open(output_path, "w") as f:
+        for state, actions in qtable.values.items():
+            f.write(f"{state}: {actions}\n")
